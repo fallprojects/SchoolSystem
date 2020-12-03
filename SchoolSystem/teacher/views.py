8590d73580ee355726fdcb86f46920065d0d449e
erjan <johnydepp2002@mail.ru>
2020-12-03 19:11:29 +0600

    add new method post and get

--- SchoolSystem/teacher/views.py
+++ SchoolSystem/teacher/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from pupils.models import Pupils
+from pupils.serializers import PupilsSerializer
 from .models import *
 from rest_framework import status
 from rest_framework.views import APIView
@@ -12,9 +13,32 @@ class TeacherView(APIView):
         serializer = TeacherSerializer(teacher)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
+    def post(self,request,*args,**kwargs):
+        serializer = TeacherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+
 class PupilstoSubjects(APIView):
     def get(self,*args,**kwargs):
-        subjects = PupilstoObjects.objects.all()
-        serializer = PupilstoSubjectsSerializer(subjects,many=True)
+        subjects = PupilstoObjects.objects.get(id=kwargs['pk'])
+        serializer = PupilstoSubjectsSerializer(subjects)
         return Response(serializer.data,status=status.HTTP_200_OK)
+
+class PupilSUbjectDetailGrade(APIView):
+
+
+    def get(self,*args,**kwargs):
+        subjects = PupilstoObjects.objects.get(id=kwargs['subject_id'])
+        pupil = Pupils.objects.get(id=kwargs['pupil_id'])
+        serializer = PupilsSerializer(pupil)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PupilsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 
