c677a8c5d7220f8153e895261ca2f5a554d64883
erjan <johnydepp2002@mail.ru>
2020-12-03 19:10:10 +0600

    add func create grade

--- SchoolSystem/pupils/serializers.py
+++ SchoolSystem/pupils/serializers.py
@@ -7,14 +7,23 @@ class GradeSerializer(serializers.ModelSerializer):
         model = Grade
         fields = ['id','subject','grade','date_created']
 
+
 class PupilsSerializer(serializers.ModelSerializer):
     grades = GradeSerializer(many=True)
     class Meta:
         model = Pupils
         fields = ['id','classes','first_name','last_name','grades']
 
+    def create(self, validated_data):
+        grade_data = validated_data.pop('grade')
+        Grade.objects.create(**grade_data)
+
 class ClassSerializer(serializers.ModelSerializer):
-    pupils = serializers.StringRelatedField(many=True)
+    pupils = PupilsSerializer(many=True)
     class Meta:
         model = Class
         fields = ['pupil_class','pupils']
+
+
+
+
