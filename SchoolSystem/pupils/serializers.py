21148203d37724f7eca6de4a43305b0ec7542cf5
erjan <johnydepp2002@mail.ru>
2020-12-03 16:23:05 +0600

    add grade in fields

--- SchoolSystem/pupils/serializers.py
+++ SchoolSystem/pupils/serializers.py
@@ -2,14 +2,12 @@ from .views import *
 from rest_framework import serializers
 
 class PupilsSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Pupils
-        fields = ['id','classes','first_name','last_name']
+        fields = ['id','classes','first_name','last_name','grade']
 
 class ClassSerializer(serializers.ModelSerializer):
     pupils = serializers.StringRelatedField(many=True)
     class Meta:
         model = Class
-        fields = ['id','pupil_class','pupils']
-
+        fields = ['pupil_class','pupils']
