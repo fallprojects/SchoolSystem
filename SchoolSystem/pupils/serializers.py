753102f17878d5bb35b960fe8844da6de7aa85c4
erjan <johnydepp2002@mail.ru>
2020-12-04 19:05:13 +0600

    create total_register and auth

--- SchoolSystem/pupils/serializers.py
+++ SchoolSystem/pupils/serializers.py
@@ -14,9 +14,14 @@ class PupilsSerializer(serializers.ModelSerializer):
         model = Pupils
         fields = ['id','classes','first_name','last_name','grades']
 
+
     def create(self, validated_data):
-        grade_data = validated_data.pop('grade')
-        Grade.objects.create(**grade_data)
+        grade_data = validated_data.pop('grades')
+        pupil = Pupils.objects.create(**validated_data)
+        for data in grade_data:
+            Grade.objects.create(pupil=pupil,**data)
+        return pupil
+
 
 class ClassSerializer(serializers.ModelSerializer):
     pupils = PupilsSerializer(many=True)
--- SchoolSystem/teacher/serializers.py
+++ SchoolSystem/teacher/serializers.py
@@ -1,6 +1,7 @@
 from .views import *
 from rest_framework import serializers
 from pupils.serializers import ClassSerializer
+from django.contrib.auth.models import User
 
 class TeacherSerializer(serializers.ModelSerializer):
     classes = ClassSerializer()
@@ -15,3 +16,14 @@ class PupilstoSubjectsSerializer(serializers.ModelSerializer):
         fields = ['id','subjects','classes']
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','password']
+
+    def create(self, validated_data):
+        password = self.validated_data.get('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
