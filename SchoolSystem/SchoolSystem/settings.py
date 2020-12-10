006776467b2c367e2b08700797aa754cb4c2c83e
Erjan18 <johnydepp2002@mail.ru>
2020-12-10 10:47:43 +0000

    lal

--- SchoolSystem/SchoolSystem/settings.py
+++ SchoolSystem/SchoolSystem/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
